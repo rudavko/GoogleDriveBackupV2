1a5fa54a972098fc8f9aeb1e9dbc73677c881e46
Andrii <andrii@rudavko.com>
2020-06-14 21:36:11 +0300

    Update getHierarchy.ts

--- src/files/getHierarchy.ts
+++ src/files/getHierarchy.ts
@@ -1,15 +1,19 @@
 import fs from 'fs'
 import path from 'path'
-
+interface FileInfo {
+  dir: fs.Dir
+  dirent?: fs.Dirent
+}
 export const getFiles = async function* (dirPath: string):
-  AsyncGenerator<fs.Dirent> {
+  AsyncGenerator<FileInfo> {
   if (!fs.existsSync(dirPath)) return
   const dir = await fs.promises.opendir(dirPath)
+  yield { dir }
   for await (let dirent of dir) {
     if (dirent.isDirectory()) {
       yield* getFiles(path.join(dir.path, dirent.name))
     } else {
-      yield dirent
+      yield { dir, dirent }
     }
   }
   return
