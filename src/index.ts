b9c99ee1684dbb7002a9592524293f7bb198980b
Andrii <andrii@rudavko.com>
2020-06-14 21:36:25 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { google } from 'googleapis'
-import { getAuth } from './auth'
+import { getAuth } from './auth/auth'
+import { uploadDirStructure } from './upload/uploadDirStructure'
+import { dirPath } from './config/dirPath'
 
+console.log('STARTED')
 getAuth()
   .then(auth => {
     if (!auth) return
     const drive = google.drive({ version: 'v3', auth })
-    return drive.about.get({ fields: 'storageQuota' })
+    return uploadDirStructure(dirPath, drive)
   })
   .then(result => console.log(result))
   .catch(er => console.log('err:', er))
